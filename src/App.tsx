fd9f65e06085bcf998d6d3c16ff4cd416a34237c
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-07-07 10:21:25 -0300

    feat: make request

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import ApiAdapter from "./shared/adapters/api/api-adapter";
+
 function App() {
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await ApiAdapter.get({ endpoint: "/todos" });
+        console.log(response);
+      } catch (error) {
+        console.error("GET TODOS ", error);
+      }
+    };
+    getData();
+  }, []);
+
   return <>Hello</>;
 }
 
