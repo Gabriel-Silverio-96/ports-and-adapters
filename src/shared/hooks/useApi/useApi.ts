a7f02cc7754c70f65cc559f00c02d66dca9eea5d
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-07-13 13:58:11 -0300

    refactor: path import

--- src/shared/hooks/useApi/useApi.ts
+++ src/shared/hooks/useApi/useApi.ts
@@ -1,6 +1,6 @@
 import AxiosAdapter from "../../api/adapters/axios";
 import FetchAdapter from "../../api/adapters/fetch";
-import ApiPort from "../../api/ports/api-port";
+import ApiPort from "../../api/ports/ApiPort";
 
 export function useApi() {
   const adapter = new AxiosAdapter();
