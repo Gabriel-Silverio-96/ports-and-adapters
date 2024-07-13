be705b449b9024c22bd5a4fdbaca986401306b83
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-07-13 15:27:36 -0300

    refactor: create file AxiosInstance

--- src/shared/api/adapters/axios/AxiosAdapter.ts
+++ src/shared/api/adapters/axios/AxiosAdapter.ts
@@ -1,8 +1,5 @@
-import axios from "axios";
-import { API } from "../../../../app.constants";
 import { HttpClientConfig, HttpClientResponse } from "../../types";
-
-const AxiosInstance = axios.create({ baseURL: API.BASE_URL });
+import AxiosInstance from "./config/AxiosInstance";
 
 export default class AxiosAdapter {
   async get<T, D>(
