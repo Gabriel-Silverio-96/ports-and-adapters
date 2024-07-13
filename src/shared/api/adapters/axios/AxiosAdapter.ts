64fd795ececca26ae6763543e46854f3218c2e50
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-07-13 15:32:44 -0300

    docs: jsdocs AxiosAdapter

--- src/shared/api/adapters/axios/AxiosAdapter.ts
+++ src/shared/api/adapters/axios/AxiosAdapter.ts
@@ -1,7 +1,19 @@
 import { HttpClientConfig, HttpClientResponse } from "../../types";
 import AxiosInstance from "./config/AxiosInstance";
 
+/**
+ * Provides methods to make HTTP requests using Axios.
+ */
 export default class AxiosAdapter {
+  /**
+   * Makes a GET request to the specified endpoint with optional configuration.
+   *
+   * @template T - The type of the response data.
+   * @template D - The type of the request data.
+   * @param {string} endpoint - The endpoint to make the GET request to.
+   * @param {HttpClientConfig<D>} [config] - Optional configuration for the request.
+   * @returns {Promise<HttpClientResponse<T>>} - A promise that resolves to the response data.
+   */
   async get<T, D>(
     endpoint: string,
     config?: HttpClientConfig<D>
