a91fa568ed5d853fd3b50849e9a8f4311281d767
Gabriel Silvério <53228013+Gabriel-Silverio-96@users.noreply.github.com>
2024-07-13 15:43:58 -0300

    refactor: axios adapter (#6)
    * refactor: create file AxiosInstance
    
    * test: class AxiosAdapter
    
    * docs: jsdocs AxiosAdapter

--- src/shared/api/adapters/axios/AxiosAdapter.ts
+++ src/shared/api/adapters/axios/AxiosAdapter.ts
@@ -1,10 +1,19 @@
-import axios from "axios";
-import { API } from "../../../../app.constants";
 import { HttpClientConfig, HttpClientResponse } from "../../types";
+import AxiosInstance from "./config/AxiosInstance";
 
-const AxiosInstance = axios.create({ baseURL: API.BASE_URL });
-
+/**
+ * Provides methods to make HTTP requests using Axios.
+ */
 export default class AxiosAdapter {
+  /**
+   * Makes a GET request to the specified endpoint with optional configuration.
+   *
+   * @template T - The type of the response data.
+   * @template D - The type of the request data.
+   * @param {string} endpoint - The endpoint to make the GET request to.
+   * @param {HttpClientConfig<D>} [config] - Optional configuration for the request.
+   * @returns {Promise<HttpClientResponse<T>>} - A promise that resolves to the response data.
+   */
   async get<T, D>(
     endpoint: string,
     config?: HttpClientConfig<D>
