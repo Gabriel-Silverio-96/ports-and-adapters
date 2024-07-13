f3b48031b739cd75010fb92283466f38da335e1b
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-07-13 15:28:01 -0300

    test: class AxiosAdapter

---
+++ src/shared/api/adapters/axios/__test__/AxiosAdapter.test.ts
@@ -0,0 +1,61 @@
+import {
+  afterEach,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  vi,
+  vitest,
+} from "vitest";
+import { HttpClientConfig } from "../../../types";
+import AxiosAdapter from "../AxiosAdapter";
+import AxiosInstance from "../config/AxiosInstance";
+
+const mockedAxios = AxiosInstance as any;
+
+vi.mock("../config/AxiosInstance");
+let adapter: AxiosAdapter;
+
+beforeEach(() => {
+  adapter = new AxiosAdapter();
+});
+
+afterEach(() => {
+  vitest.clearAllMocks();
+});
+
+describe("AxiosAdapter", () => {
+  it("should make a GET request and return data", async () => {
+    const endpoint = "/path";
+    const response = [{ id: 1 }];
+
+    mockedAxios.get.mockResolvedValue({ data: response });
+
+    const { data } = await adapter.get<typeof response, any>(endpoint);
+
+    expect(data).toEqual(response);
+    expect(mockedAxios.get).toHaveBeenCalledWith(endpoint, {});
+  });
+
+  it("should make request with config", async () => {
+    const endpoint = "/path";
+    const config: HttpClientConfig<{ Authorization: string }> = {
+      headers: { Authorization: "Bearer token" },
+    };
+
+    mockedAxios.get.mockResolvedValue({ data: {} });
+    await adapter.get(endpoint, config);
+
+    expect(mockedAxios.get).toHaveBeenCalledWith(endpoint, { ...config });
+  });
+
+  it("should handle errors", async () => {
+    const endpoint = "/path";
+    const message = "Network Error";
+
+    mockedAxios.get.mockRejectedValue(new Error(message));
+    const response = async () => await adapter.get(endpoint);
+
+    expect(response).rejects.toThrow(message);
+  });
+});
