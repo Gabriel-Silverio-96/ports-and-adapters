a8cd44fd3142d442b696730829e860f9483a50dc
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-07-14 10:35:55 -0300

    docs: jsdocs FetchAdapter

--- src/shared/api/adapters/fetch/FetchAdapter.ts
+++ src/shared/api/adapters/fetch/FetchAdapter.ts
@@ -5,7 +5,20 @@ import {
 } from "src/shared/api/types/ApiAdapter.types";
 import FetchErrorHandler from "src/shared/api/adapters/fetch/utils/fetch-error-handler";
 
+/**
+ * Provides methods to make HTTP requests using the Fetch API.
+ */
 export default class FetchAdapter {
+  /**
+   * Makes a GET request to the specified endpoint with optional configuration.
+   *
+   * @template T - The type of the response data.
+   * @template D - The type of the request data.
+   * @param {string} endpoint - The endpoint to make the GET request to.
+   * @param {HttpClientConfig<D>} [config] - Optional configuration for the request.
+   * @returns {Promise<HttpClientResponse<T>>} - A promise that resolves to the response data.
+   * @throws {Error} - Throws an error if the response status is not OK.
+   */
   async get<T, D>(
     endpoint: string,
     config?: HttpClientConfig<D>
