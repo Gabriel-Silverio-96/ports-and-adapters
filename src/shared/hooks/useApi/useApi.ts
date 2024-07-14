5e868beb173b52c35b764d2e33b7d38eba65182f
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-07-14 12:04:52 -0300

    feat: selected AxiosAdapter

--- src/shared/hooks/useApi/useApi.ts
+++ src/shared/hooks/useApi/useApi.ts
@@ -3,8 +3,8 @@ import FetchAdapter from "../../api/adapters/fetch";
 import ApiPort from "../../api/ports/ApiPort";
 
 export function useApi() {
-  //const adapter = new AxiosAdapter();
-  const adapter = new FetchAdapter();
+  const adapter = new AxiosAdapter();
+  // const adapter = new FetchAdapter();
   const api = new ApiPort(adapter);
 
   return api;
