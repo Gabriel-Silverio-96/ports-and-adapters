4a7239f633c14b8bc35ecea7db51fa04ab268fc9
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-07-18 17:32:23 -0300

    refactor: use absolute imports and jsdocs

--- src/shared/api/ports/ApiPort.ts
+++ src/shared/api/ports/ApiPort.ts
@@ -1,15 +1,27 @@
 import {
   HttpClientConfig,
   HttpClientResponse,
-} from "../types/ApiAdapter.types";
-import { HttpClient } from "./types";
+} from "src/shared/api/types/ApiAdapter.types";
+import { HttpClient } from "src/shared/api/ports/types";
 
+/**
+ * ApiPort will receive adapter.
+ * Implements the HttpClient interface.
+ */
 export default class ApiPort implements HttpClient {
   adapter: HttpClient;
   constructor(adapter: HttpClient) {
     this.adapter = adapter;
   }
-
+  /**
+   * Makes a GET request to the specified endpoint with optional configuration.
+   *
+   * @template T - The type of the response data.
+   * @template D - The type of the request data.
+   * @param {string} endpoint - The endpoint to make the GET request to.
+   * @param {HttpClientConfig<D>} [config] - Optional configuration for the request.
+   * @returns {Promise<HttpClientResponse<T>>} - A promise that resolves to the response data
+   */
   async get<T, D>(
     endpoint: string,
     config?: HttpClientConfig<D>
