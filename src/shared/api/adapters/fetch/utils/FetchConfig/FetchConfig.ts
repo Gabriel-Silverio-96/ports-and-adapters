65496e90483cfc7c2afae01d525003aa7d003d26
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-07-19 19:42:24 -0300

    feat: use JSON.stringify to payload

--- src/shared/api/adapters/fetch/utils/FetchConfig/FetchConfig.ts
+++ src/shared/api/adapters/fetch/utils/FetchConfig/FetchConfig.ts
@@ -3,11 +3,20 @@ import { HttpClientConfig } from "src/shared/api/types";
 class FetchConfig {
   static format(config: HttpClientConfig) {
     const { headers, payload } = config;
+    const body = FetchConfig.safeStringify(payload);
 
     return {
       headers,
-      body: payload,
+      body,
     };
+  }
+
+  static safeStringify(payload: unknown) {
+    try {
+      return JSON.stringify(payload);
+    } catch (error) {
+      return null;
+    }
   }
 }
 
