ff95882562cdd7dd3ac41cd977e63a65a9530678
Gabriel Silvério <53228013+Gabriel-Silverio-96@users.noreply.github.com>
2024-07-19 17:26:39 -0300

    refactor: custom hook useApi (#11)
    * refactor: create index file
    
    * docs: add jsdocs

--- src/shared/hooks/useApi/useApi.ts
+++ src/shared/hooks/useApi/useApi.ts
@@ -1,11 +1,17 @@
-import AxiosAdapter from "../../api/adapters/axios";
-import FetchAdapter from "../../api/adapters/fetch";
-import ApiPort from "../../api/ports/ApiPort";
+import { useMemo } from "react";
+import AxiosAdapter from "src/shared/api/adapters/axios";
+import ApiPort from "src/shared/api/ports/ApiPort";
+import FetchAdapter from "src/shared/api/adapters/fetch/FetchAdapter";
 
-export function useApi() {
-  const adapter = new AxiosAdapter();
-  // const adapter = new FetchAdapter();
-  const api = new ApiPort(adapter);
+/**
+ * Custom hook to initialize the API port and adapter.
+ * @returns {ApiPort} - The initialized API port with the HttpClient.
+ */
+export function useApi(): ApiPort {
+  const adapter = useMemo(() => new AxiosAdapter(), []);
+  //  const adapter = useMemo(() => new FetchAdapter(), []);
+
+  const api = useMemo(() => new ApiPort(adapter), []);
 
   return api;
 }
