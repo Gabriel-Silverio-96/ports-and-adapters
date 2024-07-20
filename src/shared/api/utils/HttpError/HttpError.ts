b7a5575b404dd6579e2008d2036f425728b65d79
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-07-20 13:20:06 -0300

    feat: add data in HttpError

--- src/shared/api/utils/HttpError/HttpError.ts
+++ src/shared/api/utils/HttpError/HttpError.ts
@@ -1,12 +1,14 @@
-import { HttpErrorResponse } from "src/shared/api/types";
+import { HttpErrorResponse } from "src/shared/api/utils/HttpError/types";
 
 class HttpError implements HttpErrorResponse {
   status: number;
   message: string;
+  data: unknown;
 
-  constructor(response: { status: number }) {
+  constructor(response: { status: number }, data?: unknown) {
     this.status = response.status;
     this.message = `An error has occurred: ${response.status}`;
+    this.data = data || null;
   }
 }
 
