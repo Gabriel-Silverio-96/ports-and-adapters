9c1ef34c8f5b51faed31255f87f35329546dcfd0
Gabriel Silvério <53228013+Gabriel-Silverio-96@users.noreply.github.com>
2024-07-27 12:20:50 -0300

    feat: add method put, patch and delete (#21)

--- src/shared/api/ports/ApiPort.ts
+++ src/shared/api/ports/ApiPort.ts
@@ -7,23 +7,20 @@ import { HttpClient } from "src/shared/api/ports/types";
 
 /**
  * ApiPort will receive adapter.
- * Implements the HttpClient interface.
+ *
+ * @template T - The type of the response data.
+ * @template D - The type of the request data.
+ * @param {string} endpoint - The endpoint to make the GET request to.
+ * @param {HttpClientConfig<D>} [config] - Optional configuration for the request.
+ * @returns {Promise<HttpClientResponse<T>>} - A promise that resolves to the response data
  */
 export default class ApiPort implements HttpClient {
   adapter: HttpClient;
   constructor(adapter: HttpClient) {
     this.adapter = adapter;
   }
-  /**
-   * Makes a GET request to the specified endpoint with optional configuration.
-   *
-   * @template T - The type of the response data.
-   * @template D - The type of the request data.
-   * @param {string} endpoint - The endpoint to make the GET request to.
-   * @param {HttpClientConfig<D>} [config] - Optional configuration for the request.
-   * @returns {Promise<HttpClientResponse<T>>} - A promise that resolves to the response data
-   */
-  async get<T, D>(
+
+  public async get<T, D>(
     endpoint: string,
     config?: HttpClientConfig<D, Config>
   ): Promise<HttpClientResponse<T>> {
@@ -32,11 +29,38 @@ export default class ApiPort implements HttpClient {
     return response;
   }
 
-  async post<T, D>(
+  public async post<T, D>(
     endpoint: string,
     config?: HttpClientConfig<D, Config>
   ): Promise<HttpClientResponse<T>> {
     const response = await this.adapter.post<T, D>(endpoint, config || {});
+
+    return response;
+  }
+
+  public async put<T, D>(
+    endpoint: string,
+    config?: HttpClientConfig<D, Config>
+  ): Promise<HttpClientResponse<T>> {
+    const response = await this.adapter.put<T, D>(endpoint, config || {});
+
+    return response;
+  }
+
+  public async patch<T, D>(
+    endpoint: string,
+    config?: HttpClientConfig<D, Config>
+  ): Promise<HttpClientResponse<T>> {
+    const response = await this.adapter.patch<T, D>(endpoint, config || {});
+
+    return response;
+  }
+
+  public async delete<T, D>(
+    endpoint: string,
+    config?: HttpClientConfig<D, Config>
+  ): Promise<HttpClientResponse<T>> {
+    const response = await this.adapter.delete<T, D>(endpoint, config || {});
 
     return response;
   }
