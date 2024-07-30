7beb901ee3647bc1d381ad40302627681b990012
Gabriel-Silverio-96 <gabriel.infoak2@gmail.com>
2024-07-30 19:50:23 -0300

    refactor: add method public and private

--- src/shared/api/adapters/fetch/utils/FetchConfig/FetchConfig.ts
+++ src/shared/api/adapters/fetch/utils/FetchConfig/FetchConfig.ts
@@ -1,9 +1,9 @@
 import { HttpClientConfig } from "src/shared/api/types";
 
 class FetchConfig {
-  static format(config: HttpClientConfig) {
+  public static format(config: HttpClientConfig) {
     const { headers, payload } = config;
-    const body = FetchConfig.safeStringify(payload);
+    const body = this.safeStringify(payload);
 
     return {
       headers,
@@ -11,7 +11,7 @@ class FetchConfig {
     };
   }
 
-  static safeStringify(payload: unknown) {
+  private static safeStringify(payload: unknown) {
     try {
       return JSON.stringify(payload);
     } catch (error) {
