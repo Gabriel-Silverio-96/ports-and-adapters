13460370aee966a8a1137bf5b9577887a74b43f0
Gabriel Silvério <53228013+Gabriel-Silverio-96@users.noreply.github.com>
2024-08-02 20:18:31 -0300

    refactor: created FetchInstance (#27)

--- src/shared/api/adapters/fetch/FetchAdapter.ts
+++ src/shared/api/adapters/fetch/FetchAdapter.ts
@@ -2,48 +2,13 @@ import {
   Config,
   HttpClientConfig,
   HttpClientResponse,
-  Method,
 } from "src/shared/api/types/ApiAdapter.types";
-import { FetchConfig } from "src/shared/api/adapters/fetch/utils/FetchConfig";
-import { API } from "src/app.constants";
-import { FetchErrorHandler } from "src/shared/api/adapters/fetch/utils/FetchErrorHandler";
-import { DEFAULT_FETCH_CONFIG } from "src/shared/api/adapters/fetch/FetchAdapter.constants";
+import { FetchInstance } from "src/shared/api/adapters/fetch/utils/FetchInstance";
 
 /**
  * Provides methods to make HTTP requests using the Fetch API.
  */
-class FetchAdapter {
-  /**
-   * Makes an HTTP request to the specified endpoint with optional configuration and method.
-   *
-   * @template T - The type of the response data.
-   * @template D - The type of the request data.
-   * @param {string} endpoint - The endpoint to make the request.
-   * @param {HttpClientConfig<D>} [config] - Optional configuration for the request.
-   * @param {string} [method] - The HTTP method to use for the request (e.g., "GET", "POST").
-   * @returns {Promise<HttpClientResponse<T>>} - A promise that resolves to the response data.
-   * @throws {HttpError} - Throws an HttpError if the response status is not OK.
-   * @private
-   */
-  private async fetch<T, D>(
-    endpoint: string,
-    config?: HttpClientConfig<D, Config>,
-    method?: Method
-  ): Promise<HttpClientResponse<T>> {
-    const { headers, ...rest } = FetchConfig.format({ ...config });
-
-    const response = await fetch(`${API.BASE_URL}${endpoint}`, {
-      method,
-      headers: { ...DEFAULT_FETCH_CONFIG.HEADERS, ...headers },
-      ...rest,
-    });
-
-    await FetchErrorHandler.responseError(response);
-
-    const data = await response.json();
-    return { data };
-  }
-
+class FetchAdapter extends FetchInstance {
   public async get<T, D>(
     endpoint: string,
     config?: HttpClientConfig<D, Config>
